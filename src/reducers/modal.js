1681489e325d9219431b3428bc98b73717f9054d
Swapnil Sengupta <dodosp2001@gmail.com>
2021-04-23 00:41:16 +0530

    Add modal for each feature

---
+++ src/reducers/modal.js
@@ -0,0 +1,13 @@
+import { SET_MODAL } from "../utils/constants";
+
+const modalReducer = (state = "", action) => {
+  const { modal } = action;
+  switch (action.type) {
+    case SET_MODAL:
+      return modal;
+    default:
+      return state;
+  }
+};
+
+export default modalReducer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,5 @@ export const SET_TRACKS = "SET_TRACKS";
 export const ADD_TRACKS = "ADD_TRACKS";
 
 export const SET_FEATURES = "SET_FEATURES";
+
+export const SET_MODAL = "SET_MODAL";
